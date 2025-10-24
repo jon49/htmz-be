8a1177d07a57a49f68b8d07f6c920bfc07b14cec
Jon Nyman <nymanjon@gmail.com>
2025-10-24 13:08:08 -0700

    Update secret message to random quotations.

--- routes.js
+++ routes.js
@@ -49,8 +49,22 @@ export async function routeHandler(url, request) {
   }
 
   if (url.pathname.endsWith("/link/")) {
+    let messages = [
+      "I have no special talent. I am only passionately curious. &mdash; Albert Einstein",
+      "The important thing is not to stop questioning. Curiosity has its own reason for existing. &mdash; Albert Einstein",
+      "Life is like riding a bicycle. To keep your balance, you must keep moving. &mdash; Albert Einstein",
+      "Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution. &mdash; Albert Einstein",
+      "Try not to become a man of success, but rather try to become a man of value. &mdash; Albert Einstein",
+      "The successful warrior is the average man, with laser-like focus. &mdash; Bruce Lee",
+      "Do not pray for an easy life, pray for the strength to endure a difficult one. &mdash; Bruce Lee",
+      "Knowing is not enough, we must apply. Willing is not enough, we must do. &mdash; Bruce Lee",
+      "Mistakes are always forgivable, if one has the courage to admit them. &mdash; Bruce Lee",
+      "Adapt what is useful, reject what is useless, and add what is specifically your own. &mdash; Bruce Lee",
+    ]
+    let randomIndex = Math.floor(Math.random() * messages.length)
+    let randomMessage = messages[randomIndex]
     let content = html("linkResponse", `
-<p id=link class="highlight"><strong>Whoever first figured out HTMZ is a genius!</strong></p>`)
+<p id=link class="highlight"><strong>${randomMessage}</strong></p>`)
 
     return getResponse(content)
   }
