419593a680dab75b816e8e375e49123a44b9fe79
Jon Nyman <nymanjon@gmail.com>
2025-10-23 23:40:29 -0700

    Initial commit. HTMZ FTW.

---
+++ js/sw-loader.js
@@ -0,0 +1,12 @@
+"use strict";
+(() => {
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker
+        .register(`/sw.js`, { type: "module" })
+        .then(_ => {
+            console.log("Service worker registered.")
+        })
+    } else {
+        alert("Service worker is not supported. Please use a modern browser.")
+    }
+})()
---
+++ routes.js
@@ -0,0 +1,76 @@
+/**
+ * @param {URL} url 
+ * @param {Request} request 
+ * @returns {Response | undefined}
+ */
+export async function routeHandler(url, request) {
+  if (url.pathname === "/new/") {
+    let data = await request.formData()
+    let item = data.get("item")
+    let now = Date.now()
+
+    let content = html("todoResponse", `
+<li id="_${now}" hz-target="#list" hz-swap="append">
+    ${item} <button form="post" formaction="/delete?id=_${now}">X</button>
+</li>
+<p id=events class=highlight>Added item &mdash; "${item}"!</p>`)
+
+    return getResponse(content)
+  }
+
+  if (url.pathname === "/delete/") {
+    let id = url.searchParams.get("id")
+    let content = html("todoResponse", `
+<template id="${id}"></template>
+<p id=events class=highlight>Removed id: ${id}</p>`)
+
+    return getResponse(content) 
+  }
+
+  if (url.pathname === "/new-row/") {
+    let now = new Date()
+    let content = html("tableResponse", `
+<template hz-target="tbody" hz-swap="append">
+  <tr><td>Yes!</td><td>${now.toLocaleDateString()} &mdash; ${now.toLocaleTimeString()}</td></tr>
+  <tr><td>No!</td><td>${now.toLocaleDateString()} &mdash; ${now.toLocaleTimeString()}</td></tr>
+</template>`)
+
+    return getResponse(content)
+  }
+
+  if (url.pathname === "/clear-table/") {
+    let content = html("tableResponse", `
+<template hz-target="tbody">
+  <tbody></tbody>
+</template>
+`)
+
+    return getResponse(content)
+  }
+
+  if (url.pathname === "/link/") {
+    let content = html("linkResponse", `
+<p id=link class="highlight"><strong>Whoever first figured out HTMZ is a genius!</strong></p>`)
+
+    return getResponse(content)
+  }
+}
+
+function html(id, content) {
+  // let now = Date.now()
+  return `
+    ${content}
+    <x-html id="${id}" hz-swap="replaceWith">
+      <template>${content}</template>
+    </x-html>
+  `
+}
+
+function getResponse(content) {
+  return new Response(content, {
+    status: 200,
+    headers: {
+      "Content-Type": "text/html"
+    }
+  })
+}
