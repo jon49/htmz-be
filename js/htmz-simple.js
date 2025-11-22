ae657a01bf5e91d9bbfcb42d85f94a4e8c4d3489
Jon Nyman <nymanjon@gmail.com>
2025-11-21 20:54:50 -0700

    Remove unneeded code.

--- js/htmz-simple.js
+++ js/htmz-simple.js
@@ -7,13 +7,10 @@ window.htmz = function htmz(frame) {
   for (let el of Array.from(doc.body.children).concat(Array.from(doc.head.children))) {
     // before, prepend, append, after
     let swap = el.getAttribute("hz-swap") ?? "replaceWith"
-    el.removeAttribute("hz-swap")
     let targetQuery = el.getAttribute("hz-target") || el.id && `#${el.id}`
-    el.removeAttribute("hz-target")
     let target = document.querySelector(targetQuery)
-    if (!target) continue
     if (el.tagName === "TEMPLATE") el = el.content.cloneNode(true)
-    target[swap]?.(el)
+    target?.[swap]?.(el)
   }
 
   frame.remove()
